4b2d6dbbe4664ef32c480d78c9137c8c5df27747
Mathias <mnssvendsen@gmail.com>
2022-04-06 20:41:40 +0200

    Wanted Poster

--- wanted.py
+++ wanted.py
@@ -1,6 +1,12 @@
-from guizero import App
+from guizero import App, Text, Picture
 
 app = App("Wanted!")
-app.bg = "yellow"
+app.bg = "#FBFBD0"
+
+wanted_text = Text(app, "WANTED")
+wanted_text.text_size = 50
+wanted_text.font = "Times New Roman"
+
+me = Picture(app, image="me.png")
 
 app.display()
